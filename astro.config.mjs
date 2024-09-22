f3a77679cd2e8e12d650f7efdbf9a38ce70fa40f
윤정현 <6235519+YuilMuil@users.noreply.github.com>
2024-09-22 19:10:26 +0900

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -31,8 +31,8 @@ const oklchToHex = (str) => {
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://fuwari.vercel.app/",
-  base: "/",
+  site: "https://yuilmuil.github.io/",
+  base: "yuil-blog",
   trailingSlash: "always",
   integrations: [
     tailwind(),
