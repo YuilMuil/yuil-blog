4bfe481ec1c070f475b2ddfeaeda431611ddc086
YuilMuil <6235519+YuilMuil@users.noreply.github.com>
2026-01-22 20:15:43 +0900

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -32,7 +32,7 @@ const oklchToHex = (str) => {
 // https://astro.build/config
 export default defineConfig({
   site: "https://blog.yuildev.net/",
-  base: "yuil-blog",
+  base: "/",
   trailingSlash: "always",
   integrations: [
     tailwind(),
